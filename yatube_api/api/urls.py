a18e297c32454c9ec85cfa6f6a348d7dff537fc1
Фарид Наурзгалиев <faridnaurzgaliev@gmail.com>
2023-01-12 23:12:33 +0400

    C:/Program Files/Git/

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -7,11 +7,11 @@ from api.views import GroupViewSet, PostViewSet, CommentViewSet
 
 
 router_v1 = routers.DefaultRouter()
-# не понял про поддержку версий из кода
-router_v1.register('api/v1/posts', PostViewSet, basename='post')
-router_v1.register('api/v1/groups', GroupViewSet, basename='group')
+
+router_v1.register('v1/posts', PostViewSet, basename='post')
+router_v1.register('v1/groups', GroupViewSet, basename='group')
 router_v1.register(
-    r'api/v1/posts/(?P<id>\d+)/comments',
+    r'v1/posts/(?P<id>\d+)/comments',
     CommentViewSet,
     basename='comment'
 )
@@ -19,5 +19,5 @@ router_v1.register(
 
 urlpatterns = [
     path('', include(router_v1.urls)),
-    path('api/v1/api-token-auth/', views.obtain_auth_token),
+    path('v1/api-token-auth/', views.obtain_auth_token),
 ]
